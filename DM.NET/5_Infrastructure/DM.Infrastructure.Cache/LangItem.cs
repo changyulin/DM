c1aa1f5422492d2a99dedac5c8c1e9e0076438b0
Chang Yulin <cjdxchangyulin@163.com>
2014-07-18 23:10:45 +0800

    add which lose

---
+++ DM.NET/5_Infrastructure/DM.Infrastructure.Cache/LangItem.cs
@@ -0,0 +1,78 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Microsoft.Practices.EnterpriseLibrary.Caching;
+
+namespace DM.Infrastructure.Cache
+{
+    public class LangItem : CacheConfigRefID<LangItem, LangCacheFactory>
+    {
+
+        protected override string ConfigSubFolder { get { return "Lang"; } }
+        private string Language { set; get; } // en, zh
+        private string Area { set; get; } // US, CN
+
+        protected override XElement GetFromSource()
+        {
+            string file = string.Format(CultureInfo.InvariantCulture, this.ConfigFileFormat, this.RefID);
+            string path = Path.Combine(this.ConfigFolder, this.ConfigSubFolder);
+            if (!string.IsNullOrEmpty(this.Language))
+            {
+                path = Path.Combine(path, this.Language);
+            }
+            if (!string.IsNullOrEmpty(this.Area))
+            {
+                path = Path.Combine(path, this.Area);
+            }
+            path = Path.GetFullPath(Path.Combine(path, file));
+            if (File.Exists(path))
+            {
+                return XElement.Load((new XPathDocument(path)).CreateNavigator().ReadSubtree());
+            }
+            else
+            {
+                return XElement.Parse("<Strings/>");
+            }
+        }
+
+        public static LangItem Get(string language, string area, string refID)
+        {
+            ICacheFactory cf = new LangCacheFactory();
+            CacheManager cache = cf.GetCacheManager();
+            string keyName = MakeKeyName(language, area, refID);
+            LangItem item = (LangItem)cache.GetData(keyName);
+            if (item != null)
+            {
+                if (item.IsExpired())
+                {
+                    item.Remove();
+                    item = null;
+                }
+            }
+
+            if (item == null)
+            {
+                item = new LangItem();
+                item.Language = language;
+                item.Area = area;
+                item.RefID = refID;
+                item.SetValue(item.GetFromSource());
+                item.Initialize();
+                if (!item.IsDefaultData() && item.ShouldCache())
+                {
+                    cache.Add(keyName, item);
+                }
+            }
+            return item;
+        }
+
+        public static string MakeKeyName(string language, string area, string refID)
+        {
+            return string.Format(CultureInfo.InvariantCulture, "{0}:{1}:{2}:{3}", typeof(LangItem).Name, language, area, refID);
+        }
+
+
+
+    }
+}
---
+++ DM.NET/5_Infrastructure/DM.Infrastructure.Cache/ScriptItem.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using DM.Infrastructure.Util.HtmlHelpers;
+using DM.Infrastructure.Util.XmlHelpers;
+
+namespace DM.Infrastructure.Cache
+{
+    public class ScriptItem : CacheConfigRefID<ScriptItem, ConfigCacheFactory>
+    {
+        protected override string ConfigSubFolder { get { return "Script"; } }
+
+        public static IEnumerable<XElement> GetJSList(string refID)
+        {
+            XElement scriptItem = ScriptItem.Get(refID).Value;
+            List<XElement> jsList = new List<XElement>();
+            if (scriptItem == null)
+            {
+                return jsList;
+            }
+            IEnumerable<XElement> scriptItems = scriptItem.Elements();
+            foreach (XElement item in scriptItems)
+            {
+                if (item.Name.LocalName == "Script" && Valid(item))
+                {
+                    jsList.Add(item);
+                }
+                else if(item.Name.LocalName == "Include")
+                {
+                    ScriptItem.Get(item.Attribute("ID").Value);
+                }
+            }
+            return jsList;
+        }
+
+        private static bool Valid(XElement jsItem)
+        {
+            string browser = XmlHelpers.GetAttribute(jsItem, "Browser");
+            bool browserPass = browser.Length == 0 || HtmlHelpers.BrowserName().Equals(browser, StringComparison.OrdinalIgnoreCase);
+            if (!browserPass)
+                return false;
+
+            // if the browser is specified, check if the version number is specified
+            if (!string.IsNullOrEmpty(browser))
+            {
+                int version;
+                if (Int32.TryParse(XmlHelpers.GetAttribute(jsItem, "BrowserMaxVersion"), out version))
+                {
+                    if (HtmlHelpers.BrowserMajorVersion() > version)
+                        return false;
+                }
+
+                if (Int32.TryParse(XmlHelpers.GetAttribute(jsItem, "BrowserMinVersion"), out version))
+                {
+                    if (HtmlHelpers.BrowserMajorVersion() < version)
+                        return false;
+                }
+            }
+
+            bool devicePass = HtmlHelpers.DeviceSupported(jsItem);
+            if (!devicePass)
+                return false;
+
+            string environment = XmlHelpers.GetAttribute(jsItem, "Environment");
+            bool environmentPass = environment.Length == 0 || ConfigurationManager.AppSettings["Environment"].Equals(environment, StringComparison.OrdinalIgnoreCase);
+
+            return environmentPass;
+        }
+    }
+}
